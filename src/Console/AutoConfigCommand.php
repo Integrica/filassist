137f3d361b70a805fba96f4ed5565b4dc5d2a083
Integrica <integrica8@gmail.com>
2024-11-27 15:19:08 +0100

    Create AutoConfigCommand.php

---
+++ src/Console/AutoConfigCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Integrica\Filassist\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Composer;
+
+class FilamentFullConfigCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'integrica:auto-config {--template= : Project template file}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Configure Filament from command line without editing the files manually and minimal input.';
+    
+    /**
+     * The Composer instance.
+     *
+     * @var \Illuminate\Support\Composer
+     */
+    protected $composer;
+
+    /**
+     * @var array{'template': string | null}
+     */
+    protected array $options;
+
+    protected string $envPath;
+    
+    public function __construct(Composer $composer)
+    {
+        parent::__construct();
+
+        $this->composer = $composer;
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->options = $this->options();
+
+        return Command::SUCCESS;
+    }
+
+}
